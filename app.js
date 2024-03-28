f389fcd2776e687d31ed8f9b0d48f2a962be2c4a
rashesh <rashesh.pithadiya.2024@gmail.com>
2024-03-28 11:48:29 +0530

    Student attendance and result exercise added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const route = require("./router/routes");
 const registrationRoute = require("./Register_Backend/router/routes");
 const jobform = require("./Job_MVC/router/router");
 const studentRec = require("./studentRecords/router/routes");
+const attendResult = require("./Attendance_27Feb/routes/router");
 const app = express();
 const cors = require("cors");
 const dotenv = require("dotenv").config();
@@ -17,6 +18,7 @@ app.use(cors());
 app.use("/", route);
 app.use("/", jobform);
 app.use("/", studentRec);
+app.use("/", attendResult);
 
 app.listen(process.env.PORT, () => {
 	console.log("http://127.0.0.1:8000");
